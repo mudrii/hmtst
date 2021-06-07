fd501acaf21a71019028bddc41b9f21053812d56
mudrii <mudreac@gmail.com>
2021-06-07 15:35:39 +0800

    reformat flake

--- flake.nix
+++ flake.nix
@@ -7,35 +7,36 @@
     home-manager.url = "github:nix-community/home-manager/release-21.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
-  #outputs = {self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
-  #outputs = {self, ... }@inputs:
-  {
-    homeConfigurations = {
-      mudrii = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-linux";
-        homeDirectory = "/home/mudrii";
-        username = "mudrii";
-        stateVersion = "21.05";
-        configuration = { config, lib, pkgs, ... }:
-       	let
-          overlay-unstable = final: prev: {
-            unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
-          };
-        in {
-	      nixpkgs.overlays = [ overlay-unstable ];
-          nixpkgs.config = { allowUnfree = true; };
-          programs.home-manager.enable = true;
-/*
-          imports = [
-            ./modules/system-management/index.nix
-          ];
-*/
-          home.packages = with pkgs; [
-            tmux
-          ];
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+    #outputs = {self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
+    #outputs = {self, ... }@inputs:
+    {
+      homeConfigurations = {
+        mudrii = inputs.home-manager.lib.homeManagerConfiguration {
+          system = "x86_64-linux";
+          homeDirectory = "/home/mudrii";
+          username = "mudrii";
+          stateVersion = "21.05";
+          configuration = { config, lib, pkgs, ... }:
+            let
+              overlay-unstable = final: prev: {
+                unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
+              };
+            in
+            {
+              nixpkgs.overlays = [ overlay-unstable ];
+              nixpkgs.config = { allowUnfree = true; };
+              programs.home-manager.enable = true;
+              /*
+                imports = [
+                ./modules/system-management/index.nix
+                ];
+              */
+              home.packages = with pkgs; [
+                tmux
+              ];
+            };
         };
       };
     };
-  };
 }
