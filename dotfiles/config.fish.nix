a5d81a38c27d56c27532998137aef33a2c01250b
mudrii <mudreac@gmail.com>
2021-06-09 22:10:04 +0800

    plugin name change

--- dotfiles/config.fish.nix
+++ dotfiles/config.fish.nix
@@ -5,16 +5,14 @@
 {
   programs.fish = {
     enable = true;
-    plugins = [
-      {
-        name = "fasd";
+    plugins = [{
+        name = "bobthefish";
         src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
           repo = "theme-bobthefish";
           rev = "626bd39b002535d69e56adba5b58a1060cfb6d7b";
           sha256 = "2d87a9bbf93d586d8ffa34d10f2a52ae5b29581505d5872fd6137c9aed412fd5";
         };
-      }
-    ];
+      }];
   };
 }
