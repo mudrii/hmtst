9cc77c5af6ae5194960efce0fa9641e349dd4883
mudrii <mudreac@gmail.com>
2021-06-10 08:52:38 +0800

    fix readline

--- dotfiles/.bashrc.nix
+++ dotfiles/.bashrc.nix
@@ -2,17 +2,18 @@
 
 {
 
-  programs.bash = {
-    enable = true;
-    historyControl = [ "erasedups" "ignoredups" "ignorespace" ];
-    historyFileSize = 1000000;
-    historySize = 100000;
-    initExtra = ''
-      source ~/.git-completion.bash
-      source ~/.git-prompt.sh
-      export INPUTRC=~/.inputrc
-      eval "$(direnv hook bash)"
-      PS1="\n\[\033[1;32m\]\e[0;31m[\[\e]0;\u@\h: \w\a\]\e[01;32m\u@\[\e[1;34m\]\h:\[\e[01;36m\]\w\[\e[01;32m\]\e[0;31m]\[\033[0m\]\$(__git_ps1)\[\e[01;32m\]\$\[\033[0m\] "
+  programs = {
+    bash = {
+      enable = true;
+      historyControl = [ "erasedups" "ignoredups" "ignorespace" ];
+      historyFileSize = 1000000;
+      historySize = 100000;
+      initExtra = ''
+        source ~/.git-completion.bash
+        source ~/.git-prompt.sh
+        export INPUTRC=~/.inputrc
+        eval "$(direnv hook bash)"
+        PS1="\n\[\033[1;32m\]\e[0;31m[\[\e]0;\u@\h: \w\a\]\e[01;32m\u@\[\e[1;34m\]\h:\[\e[01;36m\]\w\[\e[01;32m\]\e[0;31m]\[\033[0m\]\$(__git_ps1)\[\e[01;32m\]\$\[\033[0m\] "
       '';
       shellAliases = {
         ccat = "pygmentize -f terminal256 -g -P style=monokai";
@@ -47,10 +48,12 @@
         suf = "su --shell=/usr/bin/fish";
         vdir = "vdir --color=auto";
       };
+    };
+
     readline = {
       enable = true;
       bindings = {
-        "\e[A" =  "history-search-backward";
+        "\e[A" = "history-search-backward";
         "\e[B" = "history-search-forward";
         "\e[C" = "forward-char";
         "\e[D" = "backward-char";
@@ -65,6 +68,7 @@
         TAB: menu-complete
       '';
     };
+  };
 
   home = {
     file = {
