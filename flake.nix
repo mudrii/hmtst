f8204b7576e7b9b094d3e8b2023b80d5c4f9d84e
mudrii <mudreac@gmail.com>
2021-12-04 16:36:48 +0800

    update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Home Manager NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-21.05";
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
